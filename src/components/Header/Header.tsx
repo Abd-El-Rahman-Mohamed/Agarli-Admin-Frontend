289d330b4961a0c4f59481b577f4a92b9c602a9c
Abd-El-Rahman-Mohamed <webdevelopmentlj@gmail.com>
2025-07-27 16:20:32 +0300

    Header componnet is well styled now

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { User } from '../../types';
+import searchIcon from '../../assets/search.png';
+import mailIcon from '../../assets/mail.png';
+import notificationsIcon from '../../assets/notifications.png';
+import adminAvatar from '../../assets/admin-avatar.png';
 import './Header.css';
 
 interface HeaderProps {
@@ -25,7 +29,7 @@ const Header: React.FC<HeaderProps> = ({ user, onSearch, className = '' }) => {
               onChange={handleSearchChange}
             />
             <button className="search-button">
-              🔍
+              <img src={searchIcon} alt="Search" className="search-icon" />
             </button>
           </div>
         </div>
@@ -33,26 +37,29 @@ const Header: React.FC<HeaderProps> = ({ user, onSearch, className = '' }) => {
         <div className="header-actions">
           <div className="notification-icons">
             <div className="notification-item">
-              <span className="notification-icon">🛒</span>
-              <span className="notification-badge">3</span>
+              <img src={mailIcon} alt="Mail" className="notification-icon" />
+              <span className="notification-badge">2</span>
             </div>
             <div className="notification-item">
-              <span className="notification-icon">🔔</span>
-              <span className="notification-badge">1</span>
+              <img src={notificationsIcon} alt="Notifications" className="notification-icon" />
+              <span className="notification-badge">8</span>
             </div>
           </div>
 
+          <div className="header-separator"></div>
+
           <div className="user-section">
-            <div className="user-avatar">
-              {user.avatar ? (
-                <img src={user.avatar} alt={user.name} />
-              ) : (
-                <div className="avatar-placeholder">{user.name.charAt(0)}</div>
-              )}
+            <div className="user-avatar-container">
+              <div className="user-avatar">
+                <img src={adminAvatar} alt={user.name} />
+              </div>
+              <div className="user-status-indicator"></div>
             </div>
-            <div className="user-info">
-              <div className="user-name">{user.name}</div>
-              <div className="user-role">{user.role}</div>
+            <div className="user-info-container">
+              <div className="user-info">
+                <div className="user-name">{user.name}</div>
+                <div className="user-role">{user.role}</div>
+              </div>
             </div>
           </div>
         </div>
